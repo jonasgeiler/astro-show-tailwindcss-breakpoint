46cc6909d62c28261bee6e8251d8e7ed6b816585
Jonas Geiler <git@jonasgeiler.com>
2025-04-09 10:10:19 +0200

    docs: add jsdoc for app.ts export

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import type { DevToolbarApp } from "astro";
 
-// Guide: https://docs.astro.build/en/recipes/making-toolbar-apps/
-// API Reference: https://docs.astro.build/en/reference/dev-toolbar-app-reference/
+/** The dev toolbar app currently does nothing except for showing an icon. */
 export default {
-	// Do nothing.
+	// Guide: https://docs.astro.build/en/recipes/making-toolbar-apps/
+	// API Reference: https://docs.astro.build/en/reference/dev-toolbar-app-reference/
 } as DevToolbarApp;
